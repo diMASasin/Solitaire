ea404e808d478a245c77c97a484634b279a26fe7
diMASasin <35368058+diMASasin@users.noreply.github.com>
2022-12-14 17:55:26 +0500

    Save TotalScore

--- Assets/UI/Scripts/Score.cs
+++ Assets/UI/Scripts/Score.cs
@@ -9,9 +9,19 @@ public class Score : MonoBehaviour
 
     private int _value;
 
+    private const string TOTAL_VALUE = nameof(TOTAL_VALUE);
+
+    public int TotalValue
+    {
+        get { return PlayerPrefs.GetInt(TOTAL_VALUE); }
+        private set { PlayerPrefs.SetInt(TOTAL_VALUE, value); }
+    }
+
     public void AddScore()
     {
         _value += Revard;
+        TotalValue += Revard;
+        Debug.Log(TotalValue);
         _text.text = "—чет:" + _value.ToString();
     }
 }
